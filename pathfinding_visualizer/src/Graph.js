36e85e1c71fdd30811c324242cd45b3fb54db59c
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-02-26 18:52:23 +0800

    Added comments to Graph.js

--- pathfinding_visualizer/src/Graph.js
+++ pathfinding_visualizer/src/Graph.js
@@ -11,17 +11,25 @@ export class Graph {
   }
 
   addEdge(node1, node2, weight) {
+    // an edge is an object with a node and weight property.
     //this.adjList[node1.name].push({ node: node2, weight: weight });
     this.adjList[node2.name].push({ node: node1, weight: weight });
   }
 
   shortestPath(startNode, endNode) {
+    // keep track of the shortest total distance from start to each node
     let times = {};
+    // keeps track of the paths found
     let backtrace = {};
+    // a priority queue of nodes and their distances
     let pq = [];
+    // an array of nodes we have visited
     const visited = [];
 
+    // initialize the start node as having 0 distance
     times[startNode.name] = 0;
+
+    // all other nodes are initially infinitely far away
     this.nodes.forEach(node => {
       if (node.name !== startNode.name) {
         times[node.name] = Infinity;
@@ -31,14 +39,20 @@ export class Graph {
     pq.push([startNode, 0]);
     while (pq.length !== 0) {
       pq.sort((a, b) => (a[1] > b[1] ? 1 : b[1] > a[1] ? -1 : 0));
+
+      // get the closest node
       let current = pq.shift();
       let currentNode = current[0];
+
       if (currentNode.isWall) {
       } else {
+        // visit this closest node
         visited.push(currentNode.name);
         if (currentNode.name === endNode.name) {
+          // we've reached the end
           break;
         } else {
+          // conduct edge relaxation on all neighbors
           this.adjList[currentNode.name].forEach(neighbor => {
             let time = times[currentNode.name] + neighbor.weight;
             if (time < times[neighbor.node.name]) {
